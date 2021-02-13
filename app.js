b9b304258642da9ffd80b468119f6e416694958a
tamburrisam <tamburrisam@gmail.com>
2021-02-13 17:58:52 -0500

    working on displayBooks loop

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 //somehow use the name 'tutu gregorio' in your page thats mad funny
+let addBtn = document.querySelector('.addbtn');
+let displayTitle = document.querySelector('.displayTitle');
+let textBox = document.querySelector('.text-box');
+let bookCard = document.querySelector('.bookcard');
+let bookCardDisplay = document.querySelector('.afterfunctionbox')
 
+addBtn.addEventListener('click', addBookToLibrary)
 
 let myLibrary = [];
 
@@ -13,14 +19,27 @@ function Book(title,author,pages,read){
     }    
 }
 
-function addBookToLibrary(book, author, pages, read){
-    let userInput = new Book(book, author, pages, read);
+function addBookToLibrary(title, author, pages, read){
+    title = prompt('Book name'); //document.getelementbyid to get this from html input
+    author = prompt('Author name');
+    pages = prompt('Pages');
+    read = prompt('Read');
+    userInput = new Book(title, author, pages, read);
     myLibrary.push(userInput);
-
+    displayBook();
     return userInput;
-
 }
 
 
+function displayBook(){
+    for(let i = 0; i < myLibrary.length; i++){
+        let div = document.createElement('div');
 
-console.log(addBookToLibrary('3232','323222','fsd3','yes'))
+        div.innerHTML = myLibrary[myLibrary.length-1].title;
+
+        bookCardDisplay.appendChild(div);
+
+        
+    };
+    
+}
