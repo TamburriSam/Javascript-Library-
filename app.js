3f1e86985583c788c61ef095a3cd9efae2902f72
tamburrisam <tamburrisam@gmail.com>
2021-02-15 14:09:40 -0500

    tweaked functionality

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 //somehow use the name 'tutu gregorio' in your page thats mad funny
-let addBtn = document.querySelector('.addbtn');
+let addBookBtn = document.querySelector('.addbtn');
 let displayTitle = document.querySelector('.displayTitle');
 let textBox = document.querySelector('.text-box');
 let bookCard = document.querySelector('.bookcard');
-let bookCardDisplay = document.querySelector('.afterfunctionbox')
+let bookCardDisplay = document.querySelector('.afterfunctionbox');
+let bookSubmit = document.querySelector('.bookSubmit');
+let bookIcon = document.querySelector('.book');
+let removeBtn = document.querySelector('.removebutton');
+let contentBox = document.querySelector('.afterfunctionbox');
 
-addBtn.addEventListener('click', addBookToLibrary)
+
 
 let myLibrary = [];
 
@@ -20,26 +24,75 @@ function Book(title,author,pages,read){
 }
 
 function addBookToLibrary(title, author, pages, read){
-    title = prompt('Book name'); //document.getelementbyid to get this from html input
-    author = prompt('Author name');
-    pages = prompt('Pages');
-    read = prompt('Read');
+    title = document.getElementById('title').value;
+    author = document.getElementById('author').value;
+    pages = document.getElementById('pages').value
+    read = document.getElementById('checkbox').checked;
     userInput = new Book(title, author, pages, read);
     myLibrary.push(userInput);
-    displayBook();
     return userInput;
 }
 
 
 function displayBook(){
-    for(let i = 0; i < myLibrary.length; i++){
-        let div = document.createElement('div');
-
-        div.innerHTML = myLibrary[myLibrary.length-1].title;
-
-        bookCardDisplay.appendChild(div);
-
-        
-    };
     
+
+        let div1 = document.createElement('div');
+        contentBox.style.display="inline-block";
+        div1.classList.add('bookcard');
+        div1.style.display = "block"
+
+     
+
+       
+        div1.innerHTML = `Title: ${myLibrary[myLibrary.length-1].title}  Author: ${myLibrary[myLibrary.length-1].author} 
+        Pages: ${myLibrary[myLibrary.length-1].pages} 
+        Read: ${myLibrary[myLibrary.length-1].read}`;  
+        
+        
+
+        bookCardDisplay.appendChild(div1);
+
 }
+
+bookSubmit.addEventListener('click', function(e){
+ 
+  addBookToLibrary();
+  displayBook();
+  modal.style.display="none";
+  displayTitle.style.display="none";
+
+  bookIcon.style.gridColumn = '3';
+  addBookBtn.style.gridColumn = '3'
+
+})
+
+
+
+
+
+
+//MODAL
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+addBookBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
