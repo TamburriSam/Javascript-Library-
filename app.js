dab5c878e87c5909170a55c26e41befd68f71679
tamburrisam <tamburrisam@gmail.com>
2021-03-10 15:40:51 -0500

    added local storage and added classes

--- app.js
+++ app.js
@@ -12,82 +12,78 @@ let totalHeading = document.querySelector(".totalbooks");
 let bookForm = document.querySelector(".bookform");
 let myLibrary = [];
 
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  titleBook() {
+    return this.title;
+  }
+
+  restOfBook() {
+    return `${this.author}<br>${this.pages}<br>${this.read}<br>`;
+  }
+}
+
+class Store {
+  //use this to get whatever is in local storage
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+      shiftElements();
+      displayTitle.style.display = "none";
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function (book) {
+      addToLs(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(title) {
+    const books = Store.getBooks();
 
 
-
+    books.forEach(function (book, index) {
+      if (JSON.stringify(book.title) === undefined) {
+        books.splice(this, 1);
+      }
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
 
 function shiftElements() {
   bookIcon.style.gridColumn = "3";
   addBookBtn.style.gridColumn = "3";
-
   totalBox.style.display = "block";
   totalHeading.style.display = "block";
   totalBox.innerHTML = myLibrary.length;
 }
 
-class Book {
-    constructor(title, author, pages, read){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-
-    totalBook () {
-        return this.title
-    }
-}
-
-class Store {
-
-    //use this to get whatever is in local storage
-    static getBooks(){
-        let books;
-        if(localStorage.getItem('books') === null){
-            books = [];
-        }else {
-            books = JSON.parse(localStorage.getItem('books'))
-            shiftElements();
-            displayTitle.style.display = 'none'
-        }
-
-        return books
-    }
-
-
-    static displayBooks() {
-        const books = Store.getBooks();
-     
-
-        books.forEach(function(book){
-
-           //const newBook = new Book(book);
-
-           addToLs(book)
-
-        })
-
-     
-
-   
-
-    }
-
-    static addBook (book) {
-       const books = Store.getBooks();
-
-       books.push(book);
-
-       localStorage.setItem('books', JSON.stringify(books))
-    }
-
-    static removeBook(){
-
-    }
-}
-
 //DOM load event
-document.addEventListener('DOMContentLoaded', Store.displayBooks())
+document.addEventListener("DOMContentLoaded", Store.displayBooks());
+
 function addBookToLibrary(title, author, pages, read) {
   title = document.getElementById("title").value;
   author = document.getElementById("author").value;
@@ -96,82 +92,68 @@ function addBookToLibrary(title, author, pages, read) {
   userInput = new Book(title, author, pages, read);
   myLibrary.push(userInput);
 
-
-
-
   return userInput;
 }
 
-
- function addToLs(book){
-    let completed = "";
-
-
-
+function addToLs(book) {
   contentBox.style.display = "flex";
   let div1 = document.createElement("div");
   div1.classList.add("bookcard");
   div1.style.display = "block";
 
-  let title = document.createElement('div');
-  title.classList.add('title');
-  div1.appendChild(title);
-
+  let title = document.createElement("div");
+  title.classList.add("title");
 
   bookCardDisplay.appendChild(div1);
 
+  title.innerHTML = JSON.stringify(book[book.length - 1].title);
+  div1.innerHTML = `By: ${JSON.stringify(
+    book[book.length - 1].author
+  )} <br> Pages: ${JSON.stringify(book[book.length - 1].pages)}<br>`;
+  div1.prepend(title);
 
-  title.innerHTML = JSON.stringify(book[book.length-1]).split('"').join(' ').replace(/[^\w\s]/gi, '');
+  //remove from LS
+  let btn = document.createElement("button");
+  btn.innerHTML = 'Scrap? <i class="fas fa-trash">';
+  btn.className = "scrap";
+  div1.appendChild(btn);
+  bookCardDisplay.appendChild(div1);
 
+  btn.addEventListener("click", function () {
+    div1.style.display = "none";
 
-} 
+    Store.removeBook(this.parentElement.firstChild.textContent);
+  });
+}
 
-
-
-function displayBook(book) {
+function displayBook() {
   let completed = "";
 
-
-
   contentBox.style.display = "flex";
   let div1 = document.createElement("div");
   div1.classList.add("bookcard");
   div1.style.display = "block";
 
-  let title = document.createElement('div');
-  title.classList.add('title');
-  div1.appendChild(title);
-
+  let title = document.createElement("div");
+  title.classList.add("title");
 
   bookCardDisplay.appendChild(div1);
-
-
-
-
-title.innerHTML = myLibrary[myLibrary.length-1].totalBook();
-
-
 
 
 
   for (let i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i].read === true) {
-      myLibrary[i].read = "Yes";
+      myLibrary[i].read = "COMPLETED";
     } else {
-      myLibrary[i].read = "No";
+      myLibrary[i].read = "Not Completed";
     }
 
-
-    div1.appendChild(title)
-
-    if (myLibrary[i].read === "Yes") {
-      completed = document.createElement("button");
-      completed.innerHTML = "COMPLETED";
-      completed.className = "completed";
-      completed.style.color = "green";
-      div1.appendChild(completed);
-    }
   }
+
+  title.innerHTML = myLibrary[myLibrary.length - 1].titleBook();
+
+  div1.innerHTML = myLibrary[myLibrary.length - 1].restOfBook();
+  div1.prepend(title);
 
   //create scrap button
   let btn = document.createElement("button");
@@ -179,15 +161,12 @@ title.innerHTML = myLibrary[myLibrary.length-1].totalBook();
   btn.className = "scrap";
   div1.appendChild(btn);
   bookCardDisplay.appendChild(div1);
- 
 
   //REMOVE BOOK
   let removeIndex = "";
   btn.addEventListener("click", function () {
     for (let i = 0; i < myLibrary.length; i++) {
-      //splitWord = title.textContent;
       div1.style.display = "none";
-
 
       removeIndex = myLibrary
         .map(function (item) {
@@ -196,14 +175,11 @@ title.innerHTML = myLibrary[myLibrary.length-1].totalBook();
         .indexOf(title.textContent);
     }
 
-
-
     myLibrary.splice(removeIndex, 1);
     totalHeading.style.display = "block";
     totalBox.style.display = "block";
-    totalBox.innerHTML = myLibrary.length; 
-
-  
+    totalBox.innerHTML = myLibrary.length;
+    Store.removeBook(this.parentElement.firstChild.textContent);
   });
 
   //read toggle
@@ -214,8 +190,6 @@ title.innerHTML = myLibrary[myLibrary.length-1].totalBook();
 
   toggler.addEventListener("click", function () {
     for (let i = 0; i <= myLibrary.length; i++) {
-
-
       findIndex = myLibrary
         .map(function (item) {
           return item.title;
@@ -223,19 +197,19 @@ title.innerHTML = myLibrary[myLibrary.length-1].totalBook();
         .indexOf(title.textContent);
     }
 
-    if (myLibrary[findIndex].read === "No") {
+    if (myLibrary[findIndex].read === "Not Completed") {
       toggler.style.color = "green";
       toggler.innerHTML = "COMPLETE";
-      myLibrary[findIndex].read = "Yes";
+      myLibrary[findIndex].read = "COMPLETED";
       div1.innerHTML = `Title: ${myLibrary[findIndex].title} <br>
                           Author: ${myLibrary[findIndex].author} <br>
                           Pages : ${myLibrary[findIndex].pages} <br>`;
       div1.appendChild(toggler);
       div1.appendChild(btn);
-    } else if (myLibrary[findIndex].read === "Yes") {
+    } else if (myLibrary[findIndex].read === "COMPLETED") {
       toggler.style.color = "red";
       toggler.innerHTML = "Not Read";
-      myLibrary[findIndex].read = "No";
+      myLibrary[findIndex].read = "Not Completed";
       div1.innerHTML = `Title: ${myLibrary[findIndex].title} <br>
                           Author: ${myLibrary[findIndex].author} <br>
                           Pages : ${myLibrary[findIndex].pages} <br>`;
@@ -245,6 +219,7 @@ title.innerHTML = myLibrary[myLibrary.length-1].totalBook();
   });
 }
 
+//submit button to fire main events
 bookSubmit.addEventListener("click", function (e) {
   addBookToLibrary();
   displayBook();
